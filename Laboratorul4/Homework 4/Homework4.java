522c2f21fe73200acb24d3f9874d601274fdae51
stefania1570 <93086046+stefania1570@users.noreply.github.com>
2023-03-22 14:58:48 +0200

    Update Homework4.java

--- Laboratorul4/Homework 4/Homework4.java
+++ Laboratorul4/Homework 4/Homework4.java
@@ -1,24 +1,64 @@
 package org.example;
 
-import java.io.IOException;
+import com.github.javafaker.Faker;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CatalogUtil.InvalidCatalogException {
-        Catalog catalog = new Catalog();
+    public static void main(String[] args) {
+        List<Student> listOfStudents = new LinkedList<>();
+        var arrayOfStudents = IntStream.rangeClosed(0, 5)
+                .mapToObj(i -> new Student())
+                .sorted(Comparator.comparing(Student::getName))
+                .toArray(Student[]::new);
+        listOfStudents.addAll(Arrays.asList(arrayOfStudents));
 
-        Document doc1 = new Document(1, "Document 1", "d://document1.txt");
-        doc1.addTag("author", "John Mitchell");
-        doc1.addTag("year", "2003");
-        catalog.add(doc1);
+        System.out.println("Afisare studenti: *************************************************** ");
+        for (Student listElement : listOfStudents) {
+            System.out.println(listElement.getName());
+        }
 
-        Document doc2 = new Document(2, "Document 2", "c://document2.html");
-        doc2.addTag("author", "Manny Iscusitul");
-        doc2.addTag("year", "2021");
-        catalog.add(doc2);
+        Set<Project> setOfProjects = new TreeSet<>();
+        var projects = IntStream.rangeClosed(0, 8)
+                .mapToObj(i -> new Project())
+                .collect(Collectors.toCollection(TreeSet::new));
+        setOfProjects.addAll(projects);
 
-        CatalogUtil.save(catalog, "catalog.json");
+        System.out.println("\nAfisare proiecte: *************************************************** ");
+        for (Project listElement : setOfProjects) {
+            System.out.print(listElement.getName() + " ");
+        }
+        System.out.println("\n\nAfisare studenti + proiectele lor admisibile ************************");
+        //Declaram o instanta a problemei
+        Problem problemInstance = new Problem();
+        problemInstance.setStudents(listOfStudents);
+        problemInstance.setProjects(setOfProjects);
 
-        Catalog loadedCatalog = CatalogUtil.load("catalog.json");
-        System.out.println(loadedCatalog.toString());
+        //Asignare proiecte admisibile pt fiecare student
+        for (var student : problemInstance.getStudents()) {
+            Faker faker = new Faker();
+            int randomNumber = faker.number().numberBetween(1, 8);
+            System.out.print(student.getName() + ": ");
+            var subSet = setOfProjects.stream().limit(randomNumber).collect(Collectors.toSet());
+            student.setAdmissibleProjects(subSet);
+            student.getAdmissibleProjects().forEach(i -> {
+                System.out.print(i.getName() + " ");
+            });
+            System.out.print("\n");
+
+        }
+
+        Map<Student, Project> solution = new HashMap<>();
+        solution.putAll(problemInstance.assignProjectsGreedy());
+        System.out.println("\nAfisare distributie studenti-proiecte: **************************************************");
+        for (var student : solution.entrySet()) {
+            System.out.println(student.getKey().getName() + " " + student.getValue().getName());
+        }
+
+        System.out.println("\nAfisare studenti care au un numar de preferinte mai mic decat average-ul: *************");
+        problemInstance.studentsWithLowerThanAverageNumberOfPreferences();
+
     }
 }
